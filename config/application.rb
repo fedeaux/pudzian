f70e42afed13438b09a1dfe9990ff876997a95a5
Pedro Bernardes <phec06@gmail.com>
2016-09-08 12:19:28 -0300

    Fixing auth bugs

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module Pudzian
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :options],
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
       end
     end
   end

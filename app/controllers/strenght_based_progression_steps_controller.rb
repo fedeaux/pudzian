f22f4d745f811687152addb6f54b36007b82fd43
Pedro Bernardes <phec06@gmail.com>
2016-09-15 11:48:21 -0300

    DELETE strenght_based_progressions/:progression_id/strenght_based_progression_steps/:id

--- app/controllers/strenght_based_progression_steps_controller.rb
+++ app/controllers/strenght_based_progression_steps_controller.rb
@@ -20,12 +20,27 @@ class StrenghtBasedProgressionStepsController < ApplicationController
     end
   end
 
+  def destroy
+    if @strenght_based_progression.user.id == current_user.id
+      @step = StrenghtBasedProgressionStep.find params[:id]
+      @step.destroy
+      render :show
+    else
+      head 403
+    end
+  end
+
   private
   def set_strenght_based_progression
     @strenght_based_progression = StrenghtBasedProgression.find params[:strenght_based_progression_id]
   end
 
   def step_params
-    params.require(:strenght_based_progression_step).permit(:id, :done_at, :strenght_level, :planned_set, :done_set)
+    params.require(:strenght_based_progression_step).permit(:id,
+      :done_at,
+      :done_set,
+      :planned_set,
+      :strenght_level,
+    )
   end
 end
